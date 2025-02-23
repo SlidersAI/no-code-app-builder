3cd97c347324888af088226e9c953681ba6b31f7
SlidersAI <adrian.c.white79@gmail.com>
2025-02-23 16:21:27 +0000

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,1 +1,9 @@
-export default function Home() { return <h1>Welcome to No-Code App Builder</h1>; }
+import NoCodeAppBuilder from "./NoCodeAppBuilder";
+
+export default function Home() {
+  return (
+    <main>
+      <NoCodeAppBuilder />
+    </main>
+  );
+}
